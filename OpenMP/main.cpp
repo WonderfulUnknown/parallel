f51e5a8e9214f1c45945028fc399bae6aabf9f54
lin <389038968@qq.com>
2019-05-19 21:27:16 +0800

    finish openmp

--- OpenMP/main.cpp
+++ OpenMP/main.cpp
@@ -11,26 +11,23 @@
 
 using namespace std;
 
-const int N = 1024;
+const int N = 512;
 float mat[N][N], temp[N][N], result[N][N];
 
-// int my_rank = omp_get_thread_num();
-// int thread_count = omp_get_num_threads();
 #ifdef _OPENMP
 int my_rank = omp_get_thread_num();
-//int thread_count = omp_get_num_threads();
 int thread_count = 4;
 #else
 int my_rank = 0;
 int thread_count = 1;
 #endif
 
-void print(float matrix[N][N]) //输出
+void print(float mat[N][N]) //输出
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
-            cout << matrix[i][j] << " ";
+            cout << mat[i][j] << " ";
         cout << endl;
     }
 }
@@ -77,50 +74,96 @@ void normal_gauss(float mat[N][N]) //普通高斯消去法
     }
 }
 
-//OpenMP并行循环
+//OpenMP循环划分
 void test1()
 {
-    int i,j;
     for (int k = 0; k < N; k++)
     {
-        for (j = k + 1; j < N; j++)
+        for (int j = k + 1; j < N; j++)
             mat[k][j] = mat[k][j] / mat[k][k];
         mat[k][k] = 1.0;
 
-        #pragma omp parallel for num_threads(thread_count) shared(k,mat) private(i,j)
-        for (i = k + 1; i < N; i++)
+        #pragma omp parallel for
+        for (int i = k + 1; i < N; i++)
         {
-            for (j = k + 1; j < N; j++)
+            for (int j = k + 1; j < N; j++)
                 mat[i][j] = mat[i][j] - mat[i][k] * mat[k][j];
             mat[i][k] = 0;
         }
     }
 }
 
+//OpenMP循环划分,4,5行并行
 void test2()
 {
-#pragma omp parallel
-    //cout << thread_count << endl;
+    #pragma omp parallel
     for (int k = 0; k < N; k++)
     {
         #pragma omp for
         for (int j = k + 1; j < N; j++)
-        {
             mat[k][j] = mat[k][j] / mat[k][k];
-        }
         mat[k][k] = 1.0;
-        #pragma omp barrier
 
         #pragma omp for
         for (int i = k + 1; i < N; i++)
         {
-            for (int l = k + 1; l < N; l++)
-            {
-                mat[i][l] = mat[i][l] - mat[i][k] * mat[k][l];
-            }
+            for (int j = k + 1; j < N; j++)
+                mat[i][j] = mat[i][j] - mat[i][k] * mat[k][j];
             mat[i][k] = 0;
         }
-        #pragma omp barrier
+    }
+}
+
+//OpenMP循环划分，4,5行并行, SSE
+void test3()
+{
+    __m128 t1, t2, t3;
+    float a[4];
+    #pragma omp parallel private(t1,t2,t3,a)
+    for (int k = 0; k < N; k++)
+    {
+        #pragma omp for
+        for (int j = k + 1; j < N; j++)
+            mat[k][j] = mat[k][j] / mat[k][k];
+        mat[k][k] = 1.0;
+
+        #pragma omp for
+        for (int i = k + 1; i < N; i++)
+        {
+            for (int x = 0; x < 4; x++)
+                a[x] = mat[i][k];
+            t1 = _mm_loadu_ps(a);
+            for (int j = N - 4; j > k; j -= 4)
+            {
+                t2 = _mm_loadu_ps(mat[i] + j);
+                t3 = _mm_loadu_ps(mat[k] + j);
+                t3 = _mm_mul_ps(t1, t3);
+                t3 = _mm_sub_ps(t2, t3);
+                _mm_storeu_ps(mat[i] + j, t3);
+            }
+            for (int j = k + 1; j % 4; j++)
+                mat[i][j] = mat[i][j] - mat[i][k] * mat[k][j];
+            mat[i][k] = 0;
+        }
+    }
+}
+
+//OpenMP动态划分
+void test4()
+{
+    for (int k = 0; k < N; k++)
+    {
+        for (int j = k + 1; j < N; j++)
+            mat[k][j] = mat[k][j] / mat[k][k];
+        mat[k][k] = 1.0;
+
+        #pragma omp parallel for schedule(dynamic, N / thread_count)
+        for (int i = k + 1; i < N; i++)
+        {
+            for (int j = k + 1; j < N; j++)
+                mat[i][j] = mat[i][j] - mat[i][k] * mat[k][j];
+            mat[i][k] = 0;
+        }
     }
 }
 
@@ -139,7 +182,7 @@ int main()
     copy(mat, temp);
     cout << "普通高斯消去" << endl;
     QueryPerformanceCounter((LARGE_INTEGER *)&head); // start time
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i  < 10; i++)
     {
         copy(mat, result);
         normal_gauss(result);
@@ -151,28 +194,50 @@ int main()
     cout << endl;
     cout << "OpenMP + 循环划分" << endl;
     QueryPerformanceCounter((LARGE_INTEGER *)&head); // start time
-    //for (int i = 0; i < 100; i++)
-    //{
-    copy(temp, mat);
-    //#pragma omp parallel num_threads(thread_count)
-    test1();
-    //compare(result,mat);
-    //}
+    for (int i = 0; i  < 10; i++)
+    {
+        copy(temp, mat);
+        test1();
+    }
     QueryPerformanceCounter((LARGE_INTEGER *)&tail); // end time
-    //cout << "耗时：" << (tail - head) * 1000.0 / freq / 100 << "ms" << endl;
-    cout << "耗时：" << (tail - head) * 1000.0 / freq << "ms" << endl;
+    cout << "耗时：" << (tail - head) * 1000.0 / freq / 10 << "ms" << endl;
     compare(result, mat);
-    //print(mat);
 
-        cout << endl;
-    cout << "OpenMP + 循环划分" << endl;
+    cout << endl;
+    cout << "OpenMP + 循环划分 + 动态调度" << endl;
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    copy(temp, mat);
-    //#pragma omp parallel num_threads(thread_count)
-    test2();
-    QueryPerformanceCounter((LARGE_INTEGER *)&tail);
-    cout << "耗时：" << (tail - head) * 1000.0 / freq << "ms" << endl;
+    for (int i = 0; i  < 10; i++)
+    {
+        copy(temp, mat);
+        test4();
+    }
+    QueryPerformanceCounter((LARGE_INTEGER *)&tail); // end time
+    cout << "耗时：" << (tail - head) * 1000.0 / freq / 10 << "ms" << endl;
     compare(result, mat);
-    //print(mat);
+
+    cout << endl;
+    cout << "OpenMP + 循环划分 + 4,5行并行" << endl;
+    QueryPerformanceCounter((LARGE_INTEGER *)&head);
+    for (int i = 0; i  < 10; i++)
+    {
+        copy(temp, mat);
+        test2();
+    }
+    QueryPerformanceCounter((LARGE_INTEGER *)&tail); // end time
+    cout << "耗时：" << (tail - head) * 1000.0 / freq / 10 << "ms" << endl;
+    compare(result, mat);
+
+    cout << endl;
+    cout << "OpenMP + 循环划分 + 4,5行并行 + SSE" << endl;
+    QueryPerformanceCounter((LARGE_INTEGER *)&head);
+    for (int i = 0; i  < 10; i++)
+    {
+        copy(temp, mat);
+        test3();
+    }
+    QueryPerformanceCounter((LARGE_INTEGER *)&tail); // end time
+    cout << "耗时：" << (tail - head) * 1000.0 / freq / 10 << "ms" << endl;
+    compare(result, mat);
+
     return 0;
 }
