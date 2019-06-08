2099181754c6bbe756d005ddd905788171a00a8a
lin <389038968@qq.com>
2019-06-08 17:49:17 +0800

    grammar is correct,result is different

--- MPI/mpi.cpp
+++ MPI/mpi.cpp
@@ -70,7 +70,7 @@ void normal_gauss() //普通高斯消去法
 
 int main(int argc, char *argv[])
 {
-    MPI_Status status;
+    /*MPI_Status status;*/
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);  //报告识别调用进程的rank
     MPI_Comm_size(MPI_COMM_WORLD, &counts); //报告进程数
@@ -89,53 +89,38 @@ int main(int argc, char *argv[])
         /*print(answer);*/
         copy(temp, mat);
     }
-
-    float msg[N];
     int block_size = N / counts;
 
     srand(time(0));
     struct timeval tstart, tend;
     gettimeofday(&tstart, NULL);
-    for (int k = 0; k < N; k++)
-    {
-        if (my_id == 0)
-        {
-            /*0号进程把矩阵传递给其他进程*/
-            for (int i = 1; i < counts; i++)
-                for (int j = i * (N / counts); j < (i + 1) * (N / counts); j++)
-                    MPI_Send(mat[j], N, MPI_FLOAT, i, 99, MPI_COMM_WORLD);
-        }
-        else
-        {
-            for (int i = 0; i < N / counts; i++)
-                MPI_Recv(mat[i], N, MPI_FLOAT, 0, 99, MPI_COMM_WORLD, &status);
-        }
-    }
 
-    int m = 0;
+    /*MPI_Barrier(MPI_COMM_WORLD);//所有语句都执行了该调用才继续*/
+    /*把矩阵广播给所有进程*/
+    MPI_Bcast(mat, N * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    int begin = my_id * block_size;
+    int end = (my_id + 1) * block_size;
     for (int k = 0; k < N; k++)
     {
-        if (k < (my_id + 1) * block_size && k >= my_id * block_size)
+        if (k >= begin && k < end)
         {
             for (int j = k + 1; j < N; j++)
-                mat[m][j] = mat[m][j] / mat[m][k];
-            mat[m][k] = 1;
-            for (int j = 0; j < N; j++)
-                msg[j] = mat[m][j];
-            m++; //保证按序，需要考虑是否有其他更好的写法
+                mat[k][j] = mat[k][j] / mat[k][k];
+            mat[k][k] = 1;
         }
-        /*将计算好的结果广播*/
-        MPI_Bcast(msg, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
-        if (k < (my_id + 1) * block_size)
+        /*将自己计算好的结果广播*/
+        MPI_Bcast(mat[k], N, MPI_FLOAT, my_id, MPI_COMM_WORLD);
+
+        for (int i = k + 1; i < N; i++)
         {
-            for (int i = m; i < block_size; i++)
+            if (i >= begin && i < end)
             {
                 for (int j = k + 1; j < N; j++)
-                    mat[i][j] = mat[i][j] - mat[i][k] * msg[j];
+                    mat[i][j] = mat[i][j] - mat[i][k] * mat[k][j];
                 mat[i][k] = 0;
             }
-            MPI_Bcast(mat[k], N, MPI_FLOAT, 0, MPI_COMM_WORLD);
         }
+        MPI_Bcast(mat[k], N, MPI_FLOAT, my_id, MPI_COMM_WORLD);
     }
     if (my_id == 0)
     {
